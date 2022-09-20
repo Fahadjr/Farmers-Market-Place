3dda0b0ff6e25c854b8f42a4214e1a0a0df6f925
PATEL FAHAD <patelfahad79@gmail.com>
2022-09-20 15:14:29 -0400

    Finalise
    Farmers Market Place

---
+++ models/category.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+// Category Schema
+var CategorySchema = mongoose.Schema({
+   
+    title: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String
+    }
+    
+});
+
+var Category = module.exports = mongoose.model('Category', CategorySchema);
